93cece2b5f09aa333a93b174e48ac2e94c7e411a
Nguyen <e1446191@cmaisonneuve.qc.ca>
2024-03-13 11:11:26 -0400

    voyage tp1 debut

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header();?>
     <div id="entete" class="global">
       <section class="hero">
-        <h1>INDEX PHP</h1>
+        <h1>FRONT PAGE-PAGE</h1>
         <h2>4W4 - Conception d'interface <span>et développement web</span></h2>
         <h3>TIM - Collège de Maisonneuve</h3>
         <button>Événements</button>
@@ -28,20 +28,14 @@
         if(have_posts()):
           while(have_posts()): the_post();
           $titre = get_the_title();
-          $sigle = substr($titre,0,7);
-          $pos_parenthese = strpos($titre, '(');
-          $duree = substr($titre, $pos_parenthese+1,-1);
-          $titre = substr($titre, 7, $pos_parenthese-7);      
-          
+
           ?>
           <div class="carte">
-            <h3><?php echo $sigle; ?></h3>
+
             
             <h3><?php echo $titre; ?></h3>
 
             <p><?php echo wp_trim_words(get_the_content(),25); ?></p>
-
-            <p><?php echo $duree; ?></p>
             
 
 
