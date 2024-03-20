ee93bbb18d6a3db35b009b1f68dffe7080059273
Nguyen <e1446191@cmaisonneuve.qc.ca>
2024-03-20 11:17:49 -0400

    menu quand on reduit lecran

--- category.php
+++ category.php
@@ -1,22 +1,11 @@
-    <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@600&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Prompt:wght@600&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Protest+Riot&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Spicy+Rice&display=swap" rel="stylesheet">
 <?php get_header();?>
-    <div id="entete" class="global">
-      <section class="hero">
-        <h1><?php echo get_bloginfo('name') ?></h1>
-        <h2><?php echo get_bloginfo('description') ?></h2>
-        <h3>TIM - Collège de Maisonneuve</h3>
-        <button>Événements</button>
-      </section>
-      <?php
-            get_template_part('gabarit/vague');
-          ?>
-      </div>
+    
       <div id="accueil" class="global">
       <section>
-      <h2>Accueil</h2>
-      <h3>Destinations de voyage</h3>
+      <h3>Destinations par catégorie</h3>
         <div class="cours">
         <?php
      
@@ -42,20 +31,9 @@
       </section>
     </div>
        
-      <div id="evenement" class="global diagonal">
-        <section>
-          <h2>Événement (h2)</h2>
-          <h3>Horaire (h3)</h3>
-          <h4>Thème du jour (h4)</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit ligula vel quam fringilla, ac sagittis felis lacinia.</p>
-
-          <a href="" class="button-link">Événement</a>
-        </section>
-      </div>
       <div id="galerie" class="global">
         <section>
           <h2>Galerie (h2)</h2>
-          <h3>Images (h3)</h3>
           <a href="" class="button-link">Galerie</a>
         </section>
       </div>
